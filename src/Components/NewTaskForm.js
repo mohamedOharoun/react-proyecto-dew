c45d877661a6201033802960e49e258457e2bfe3
Mohamed <mohanedhzfp@gmail.com>
2022-02-04 09:34:29 +0000

    TaskLists

--- src/Components/NewTaskForm.js
+++ src/Components/NewTaskForm.js
@@ -1,11 +1,15 @@
-export const NewTaskForm = () => {
+import { useState } from "react";
 
+export const NewTaskForm = () => {
+    const maxChars = 250;
+    const [counter, setCounter] = useState(0);
+    const [isInValid, setIsInValid] = useState(false);
     return (
         <div className="col-md-3 d-flex flex-column justify-content-center">
             <form className="form needs-validation">
                 <div className="form-control d-flex flex-column">
 
-                    <div>
+                    <div className="d-flex flex-column">
                         <label htmlFor="name" className="form-label align-self-center">Name</label>
                         <input className="form-control" type="text" id="name" name="name" minLength={3}/>
                         <div className="invalid-feedback">It can only be 250 characters long</div>
@@ -13,10 +17,11 @@ export const NewTaskForm = () => {
 
                     <br/>
 
-                    <div>
-                        <label htmlFor="Task Content" className="visually-hidden">Task Content</label>
-                        <textarea className="form-control" htmlFor="content" required></textarea>
-                        <div className="invalid-feedback">It can only be 250 characters long</div>
+                    <div className="d-flex flex-column">
+                        <label htmlFor="content" className="visually-hidden">Task Content</label>
+                        <textarea rows="10" className={"form-control"  + (isInValid ? " is-invalid" : "")} htmlFor="content" onChange={(e) => {setCounter(e.target.value.length); e.target.value.length > maxChars ? setIsInValid(true) : setIsInValid(false)}} required></textarea>
+                        <div className="form-text align-self-end">{counter}/{maxChars}</div>
+                        <div className="invalid-feedback">It can only be {maxChars} characters long{isInValid}</div>
                     </div>
 
                     <br/>
--- src/Components/TasksList.js
+++ src/Components/TasksList.js
@@ -1,5 +1,50 @@
-export const TasksList = () => {
-    return (<div className="col-5">2</div>)
+import {useState, useEffect} from 'react';
+
+export const TasksList = (token) => {
+    const [tasks, setTasks] = useState([]);
+
+    const getTasks = async () => {
+        const tasks = await fetch('http://localhost:3030/api/v1/tasks', {
+                method: 'GET',
+                headers: new Headers({
+                    'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MWY2Y2Y2MjQ5ZGYzOGY0OGI0ODNmZjYiLCJuYW1lIjoiVXNlciA1IiwiaWF0IjoxNjQzNTY3MDY1LCJleHAiOjE2NDYxNTkwNjV9._fD8BVug_dBIunfkKNKx5mglbLqG5_32SgNUCB-ceOs'
+                })
+            })
+            .then(res => res.json())
+            .then(res => res.tasks);
+        setTasks(tasks);
+    };
+
+    useEffect(() => {
+        getTasks();
+    }, []);
+
+    return <div className="col-5">
+    {tasks.map((task) => {
+        return(
+            <article key={task._id} className="card">
+                <div className="d-flex flex-column card-body">
+                    <div className="d-flex justify-content-between">
+                        <h4 className="align-self-start">{task.name}</h4>
+
+                        <div className={"align-self-end btn btn-"+ (task.completed ? 'success' : 'primary')}>{task.completed ? 'done' : 'completed'}</div>
+                    </div>
+                    
+
+                    <div>{task.content}</div>
+
+                    
+    
+               <div className="d-flex justify-content-between">
+                    <button className="btn btn-primary align-self-start">Edit</button>
+                    <button className="btn btn-danger align-self-end">Delete</button> 
+               </div>
+                </div>
+               
+            </article>
+        );
+    })}
+    </div>
 }
 
 export default TasksList;
